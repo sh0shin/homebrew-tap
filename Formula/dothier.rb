b5966deb3cafc0e6846d72912c70122380e8841f
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-17 10:42:20 +0100

    Version 0.0.1

--- Formula/dothier.rb
+++ Formula/dothier.rb
@@ -1,7 +1,13 @@
+# dothier homebrew formula
 class Dothier < Formula
-  desc "dothier"
+  desc "Dotfiles management with dothier"
   homepage "https://github.com/sh0shin/dothier"
+  # dothier using github tags
+  url "https://github.com/sh0shin/dothier/archive/0.0.1.tar.gz"
+  sha256 "20a858cb2acc949c79c768efb7cb6c2347b73e0bc04b20b60a9dde1190c94528"
+  license "BSD-3-Clause"
   head "https://github.com/sh0shin/dothier.git"
+
   bottle :unneeded
 
   def install
@@ -9,6 +15,6 @@ class Dothier < Formula
   end
 
   test do
-    system "#{bin}/dothier -h"
+    system "#{bin}/dothier", "-h"
   end
 end

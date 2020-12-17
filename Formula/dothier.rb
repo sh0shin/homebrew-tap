dbc2e61ff5c4463269b74e69609b499d21f7aa00
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-17 11:05:14 +0100

    Fixed v0.0.2

--- Formula/dothier.rb
+++ Formula/dothier.rb
@@ -4,7 +4,7 @@ class Dothier < Formula
   homepage "https://github.com/sh0shin/dothier"
   # dothier using github tags
   url "https://github.com/sh0shin/dothier/archive/0.0.2.tar.gz"
-  sha256 "521d393b4e08c793be2130b77d363472715a33565f593b9f9ec141a5aadd3d58"
+  sha256 "9041276872b87c02f778c2bf87636c012d0df7b186be9b7e1fc7d98533360528"
   license "BSD-3-Clause"
   head "https://github.com/sh0shin/dothier.git"
 
@@ -12,9 +12,11 @@ class Dothier < Formula
 
   def install
     bin.install "dothier"
+    #bin.install "dotinit"
   end
 
   test do
     system "#{bin}/dothier", "-h"
+    #system "#{bin}/dotinit", "-h"
   end
 end

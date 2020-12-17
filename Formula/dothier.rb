26a51eabffa9d231d3827227f935034d24bbc4c1
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-17 10:50:06 +0100

    Version udpate testing

--- Formula/dothier.rb
+++ Formula/dothier.rb
@@ -3,8 +3,8 @@ class Dothier < Formula
   desc "Dotfiles management with dothier"
   homepage "https://github.com/sh0shin/dothier"
   # dothier using github tags
-  url "https://github.com/sh0shin/dothier/archive/0.0.1.tar.gz"
-  sha256 "20a858cb2acc949c79c768efb7cb6c2347b73e0bc04b20b60a9dde1190c94528"
+  url "https://github.com/sh0shin/dothier/archive/0.0.2.tar.gz"
+  sha256 "521d393b4e08c793be2130b77d363472715a33565f593b9f9ec141a5aadd3d58"
   license "BSD-3-Clause"
   head "https://github.com/sh0shin/dothier.git"
 

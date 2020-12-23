8c2cbb9be2504ec6ed1476fde528bddd90586661
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-23 15:09:28 +0100

    WIP macosetup

---
+++ Formula/macosetup.rb
@@ -0,0 +1,25 @@
+# macosetup homebrew formula
+class Macosetup < Formula
+  desc "Bash script based macOS setup environment"
+  homepage "https://sh0shin.org/macosetup"
+  # macosetup using github tags
+  #url "https://github.com/sh0shin/macosetup/archive/v0.0.1.tar.gz"
+  #sha256 ""
+  license "BSD-3-Clause"
+  head "https://github.com/sh0shin/macosetup.git", branch: "devel"
+
+  bottle :unneeded
+
+  def install
+    pkgshare.install "applications"
+    pkgshare.install "homebrew"
+    pkgshare.install "misc"
+    pkgshare.install "shell"
+    pkgshare.install "system_preferences"
+    bin.install "macosetup"
+  end
+
+  test do
+    system "#{bin}/macosetup", "-h"
+  end
+end

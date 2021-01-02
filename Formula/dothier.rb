3cc53d8493c46d35136410d32a388d8f78031471
Chris Frage <git@sh0shin.org>
2021-01-02 18:35:48 +0100

    Updated to version v0.1.0-beta.5

--- Formula/dothier.rb
+++ Formula/dothier.rb
@@ -3,8 +3,8 @@ class Dothier < Formula
   desc "Dotfiles management with dothier"
   homepage "https://sh0shin.org/dothier"
   # dothier using github tags
-  url "https://github.com/sh0shin/dothier/archive/v0.1.0-beta.4.tar.gz"
-  sha256 "2215f6097e933da5f24b148ed4fe9cadbc8d9e4b2b0d18e4a48ef35d661d9ea9"
+  url "https://github.com/sh0shin/dothier/archive/v0.1.0-beta.5.tar.gz"
+  sha256 "d7d0a22f1460acbc1b67c7a9b9d18dd747ea897bf058655f820edb5fa95bde2f"
   license "BSD-3-Clause"
   head "https://github.com/sh0shin/dothier.git"
 

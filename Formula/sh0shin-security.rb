3fcb98bb9df2d0b59aa17ef9334b5686eaee92b5
Chris Frage <git@sh0shin.org>
2022-10-08 08:30:15 +0200

    feat: add openssh for fido2

--- Formula/sh0shin-security.rb
+++ Formula/sh0shin-security.rb
@@ -7,11 +7,13 @@ class Sh0shinSecurity < Formula
   sha256 Digest::SHA256.file(File.expand_path(__FILE__)).hexdigest
   license "BSD-3-Clause"
   version "1"
-  revision 0
+  revision 1
 
   depends_on "gnupg"
   depends_on "pass"
   depends_on "pass-otp"
+
+  depends_on "openssh"
 
   keg_only "it's used to install my security requirements only"
 

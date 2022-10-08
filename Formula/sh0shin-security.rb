3cbd0e8e72297f03c715d44043aed23773e6b8ce
Chris Frage <git@sh0shin.org>
2022-10-08 08:21:01 +0200

    refactor!: move some base to security

---
+++ Formula/sh0shin-security.rb
@@ -0,0 +1,21 @@
+require "digest"
+
+class Sh0shinSecurity < Formula
+  desc "My security requirements"
+  homepage "https://sh0shin.org/"
+  url 'file://'+File.expand_path(__FILE__)
+  sha256 Digest::SHA256.file(File.expand_path(__FILE__)).hexdigest
+  license "BSD-3-Clause"
+  version "1"
+  revision 0
+
+  depends_on "gnupg"
+  depends_on "pass"
+  depends_on "pass-otp"
+
+  keg_only "it's used to install my security requirements only"
+
+  def install
+    bin.install_symlink libexec/"sh0shin-security"
+  end
+end
